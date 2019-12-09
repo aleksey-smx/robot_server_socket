71228457158736da64b9bf59a6b23e42c700938d
Aleksey Tatarintsev <semaxdev@gmail.com>
2019-12-10 00:50:17 +0200

    Update robot.py
    fixed tab

--- robot.py
+++ robot.py
@@ -29,48 +29,48 @@ while True:
 		print type(rawnew)
 
 		if raw[0] == rawnew[0]:
-                print 'robot moves forward'
-                GPIO.output(33, True)
-                GPIO.output(11, False)
-                GPIO.output(13, True)
-                GPIO.output(15, False)
-				time.sleep(1)
+			print 'robot moves forward'
+			GPIO.output(33, True)
+			GPIO.output(11, False)
+			GPIO.output(13, True)
+			GPIO.output(15, False)
+			time.sleep(1)
 
-        elif raw[0] == rawnew[1]:
-                print 'robot stop'
-                GPIO.output(33, False)
-                GPIO.output(11, False)
-                GPIO.output(13, False)
-                GPIO.output(15, False)
-				time.sleep(1)
+		elif raw[0] == rawnew[1]:
+			print 'robot stop'
+			GPIO.output(33, False)
+			GPIO.output(11, False)
+			GPIO.output(13, False)
+			GPIO.output(15, False)
+			time.sleep(1)
 
-        elif raw[0] == rawnew[2]:
-                print "robot moves backward"
-                GPIO.output(33, False)
-                GPIO.output(11, True)
-                GPIO.output(13, False)
-                GPIO.output(15, True)
-				time.sleep(1)
+		elif raw[0] == rawnew[2]:
+			print "robot moves backward"
+			GPIO.output(33, False)
+			GPIO.output(11, True)
+			GPIO.output(13, False)
+			GPIO.output(15, True)
+			time.sleep(1)
 
-        elif raw[0] == rawnew[3]:
-                print "robot moves left"
-                GPIO.output(33, False)
-                GPIO.output(11, True)
-                GPIO.output(13, True)
-                GPIO.output(15, False)
-				time.sleep(1)
+		elif raw[0] == rawnew[3]:
+			print "robot moves left"
+			GPIO.output(33, False)
+			GPIO.output(11, True)
+			GPIO.output(13, True)
+			GPIO.output(15, False)
+			time.sleep(1)
 
-        elif raw[0] == rawnew[4]:
-                print "robot moves right"
-                GPIO.output(33, True)
-                GPIO.output(11, False)
-                GPIO.output(13, False)
-                GPIO.output(15, True)
-				time.sleep(1)
-        else:
-                print "message error"
-                GPIO.output(33, False)
-                GPIO.output(11, False)
-                GPIO.output(13, False)
-                GPIO.output(15, False)
-				time.sleep(1)
+		elif raw[0] == rawnew[4]:
+			print "robot moves right"
+			GPIO.output(33, True)
+			GPIO.output(11, False)
+			GPIO.output(13, False)
+			GPIO.output(15, True)
+			time.sleep(1)
+		else:
+			print "message error"
+			GPIO.output(33, False)
+			GPIO.output(11, False)
+			GPIO.output(13, False)
+			GPIO.output(15, False)
+			time.sleep(1)
